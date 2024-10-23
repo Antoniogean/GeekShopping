2cee094d535629a4a1c738d4e0fddeebf0ad41c7
Antonio Gean <antgean@gmail.com>
2024-10-22 21:43:58 -0300

    Adicionar arquivos de projeto.

---
+++ GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext() { }
+
+        public MySqlContext(DbContextOptions options) : base(options) 
+        {            
+        }
+
+        public DbSet<Products> Products { get; set; }
+    }
+}
